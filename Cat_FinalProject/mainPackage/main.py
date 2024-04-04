41d40fe04330ade917087009ccf79492ea784938
Trehu <Trehu@LAPTOP-2ALAK15K.wifi.uc.edu>
2024-04-04 13:47:09 -0400

    Creating modules

---
+++ Cat_FinalProject/mainPackage/main.py
@@ -0,0 +1,18 @@
+#main.py
+#*********************************************************************************************************
+# Names: Trevor Hunt, [], [], []                                                    *
+# emails: huntt3@mail.uc.edu, [], [], []                                 *
+# Assignment Number: Final Project                                                                       *
+# Due Date: 04/23/2024                                                                                   *
+# Course/Section: IS4010-001                                                                             *
+# Semester/Year: Spring 2024                                                                             *
+# Brief Description of the assignment: []                      *
+#                                                                                                        *
+# Brief Description of what this module does: [] *
+# Citations:                                                       *
+# Anything else that's relevant:                                                                         *
+#*********************************************************************************************************
+import json
+if __name__ == "__main__":
+    pass
+    
