7736e73dc707dd8bffc834f7e59c705d4e152c3f
Nosag <Nosag@LAPTOP-9NDUEBEP.wifi.uc.edu>
2024-04-22 18:24:37 -0400

    Commit #1

--- Cat_FinalProject/mainPackage/main.py
+++ Cat_FinalProject/mainPackage/main.py
@@ -16,19 +16,29 @@ import json
 from jsonPackage.json import *
 from txtPackage.txt import txtToList
 from imagePackage.image import *
+from decryptPackage import *
+from decryptPackage.decrypt import Decrypt_List, Fernet, Fernet_Decrypt
 if __name__ == "__main__":
     text_list = txtToList("UCEnglish.txt")
     list_one = jsonToList("EncryptedGroupHints Spring 2024 Section 001-1.json")
-    cat_list = list_one["Cat"]
-    decoded_list_one = []
+    uncoded_list = Decrypt_List(text_list,list_one)
+    print(uncoded_list)
+    msg = jsonToList("TeamsAndEncryptedMessagesForDistribution - 001.json")
+    str_msg = msg[0]
+    uncoded_msg = Fernet_Decrypt(str_msg)
+    print(uncoded_msg)
     
+    '''
     for num in cat_list:
         newint = int(num)
         decoded_list_one.append(text_list[newint])
     print(decoded_list_one)
     
+    
     list_two = jsonToList("TeamsAndEncryptedMessagesForDistribution - 001.json")
     key = list_two["Cat"]
     print(key)
+    '''
+    
     
     loadImage("Cat_FinalProject.jpg").show()
